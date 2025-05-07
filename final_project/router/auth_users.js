bcc5ab822b55c1746cc7e6df33a59e17bf218082
renben-dev <bidone.renzo@gmail.com>
2025-05-07 17:06:42 -0400

    rolling update

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -87,7 +87,47 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = (req.params.isbn || '').trim();
+  const reviewer = req.session.username;
+  const rating = parseInt(req.body.rating);
+  const reviewText = (req.body.review || '').trim();
+  const selBook = books[isbn];
+
+  if(!reviewer){
+    return res.status(400).send("You must be logged in and have a non-expired session running to post reviews. Please login");
+  }
+
+  if(!selBook){
+    return res.status(400).send("Invalid ISBN");
+  }
+
+  if(isNaN(rating) || !Number.isInteger(rating) || rating<0 || rating>5) {
+    return res.status(400).send("Invalid rating. It must be an interger between 0 and 5");
+  }
+
+  if(reviewText.length < 5) {
+    return res.status(400).send("Reviews must have at least 5 characters");
+  }
+
+  const existingReview = selBook.reviews.find((review) => review.reviewer === reviewer);
+
+  if(existingReview) {
+    existingReview.rating = rating;
+    existingReview.review = reviewText;
+    return res.send("Dear " + reviewer +", your review has been updated");
+  }
+  else {
+    selReviews = selBook.reviews;
+    newReviewKey = Math.max(0, ...Object.keys(selReviews).map(Number));
+    selReviews[newReviewKey] = {
+        "reviewer"  :   reviewer,
+        "rating"    :   rating,
+        "review"    :   reviewText
+    };
+    return res.send("Dear " + reviewer + " you review has been added. Thank you for contributibg");
+  }
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.authenticated = regd_users;
