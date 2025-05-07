b145fc4138677107c0189631766d92e93ade8443
renben-dev <bidone.renzo@gmail.com>
2025-05-07 12:31:06 -0400

    rolling update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,7 +21,9 @@ public_users.get('/',function (req, res) {
             break;  
         }
     }
-    if(booksExist > 0) {
+    if(booksExist) {
+        //this is to preserve identation and stringify formatting
+        res.setHeader('Content-Type', 'text/plain');
         return res.send(JSON.stringify(books,null,4));
     } else {
         return res.send("We are revamping our store, book list will be available soon.");
@@ -33,13 +35,43 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  if(isbn) {
+    isbn = isbn.trim();
+    const selBook = books[isbn];
+    if(selBook){
+        return res.send(selBook);
+    } else {
+        return res.status(404).send("No books exist with ISBN = " + isbn);
+    }
+  }
+  else {
+    return res.status(400).send("No ISBN provided. Please send ISBN in GET request.");
+  }
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  if(author) {
+    author = author.trim();
+    //const selBooks = const book = Object.values(books).filter(book => book.author === author);
+    //case insensitive search and 'contains' search
+    const selBooks = Object.values(books).filter(book =>
+        book.author.toLowerCase().includes(author.toLowerCase())
+      );
+    if(selBooks){
+        return res.send(selBooks);
+    } else {
+        return res.status(404).send("No books exist with author containing '" + author+"'");
+    }
+  }
+  else {
+    return res.status(400).send("No author provided. Please send author in GET request.");
+  }
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
